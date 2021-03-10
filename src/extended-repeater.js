3fb0a913b48993adf93734ddac0773a932247ea8
mrkavalsky <mr.kavalsky@gmail.com>
2021-03-10 15:44:32 +0300

    feat: add the solution to extended-repeater subtask

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addRepStr = '';
+
+  if( !(options.addition === undefined) ) {
+
+    addRepStr = '' + options.addition;
+
+    let additionSeparator = options.additionSeparator || '|';
+    let addSepPlusStr = additionSeparator + addRepStr;
+
+    for(let i = 1; i < options.additionRepeatTimes; i++) {
+      addRepStr += addSepPlusStr;
+    };
+  }
+    
+
+  let string = '' + str + addRepStr;
+  let separator = options.separator || '+';
+  let sepPlusStr = separator + string;
+
+  for(let j = 1; j < options.repeatTimes; j++) {
+    string += sepPlusStr;
+  };
+
+  return string;
 };
   
